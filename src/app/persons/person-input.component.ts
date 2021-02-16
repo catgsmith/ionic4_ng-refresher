9754370fba1f05cf1edd25055d9872967dd067e8
catgsmith <catgsmith@hotmail.com>
2021-02-16 17:10:19 +0000

    two way binding

--- src/app/persons/person-input.component.ts
+++ src/app/persons/person-input.component.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-person-input',
   templateUrl: './person-input.component.html',
   styleUrls: ["./person-input.component.css"]
 })
 export class PersonInputComponent implements OnInit {
-
+  enteredPersonName = '';
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  onCreatePerson(personName: string): void {
-    console.log('Created a person called ' + personName);
+  onCreatePerson(): void {
+    console.log('Created a person called ' + this.enteredPersonName);
+    this.enteredPersonName = '';
   }
 }
