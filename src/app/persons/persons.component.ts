755ecae3e44fed360f2ecea709eeeb712b57d891
catgsmith <catgsmith@hotmail.com>
2021-02-19 15:11:43 +0000

    inject PersonsService into component

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -1,13 +1,15 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { PersonsService } from './persons.service';
 
 @Component({
   selector: 'app-persons',
   templateUrl: './persons.component.html'
 })
-export class PersonsComponent implements OnInit {
-  @Input() personList: string[];
-  constructor() { }
+export class PersonsComponent {
+  personList: string[];
 
-  ngOnInit(): void {
+  constructor(prsService: PersonsService) {
+    this.personList = prsService.persons;
   }
+
 }
