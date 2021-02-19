95af593782cd30da845664cb3389bff648b19f16
catgsmith <catgsmith@hotmail.com>
2021-02-19 15:31:26 +0000

    Use ngOnInit to get data from service

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PersonsService } from './persons.service';
 
 @Component({
   selector: 'app-persons',
   templateUrl: './persons.component.html'
 })
-export class PersonsComponent {
+export class PersonsComponent implements OnInit{
   personList: string[];
+  //private prsnService: PersonsService; 
 
-  constructor(prsService: PersonsService) {
-    this.personList = prsService.persons;
+  constructor(private prsService: PersonsService) {
+    //this.prsnService = prsnService; // adding private into constructor is shortcut for this assignment
   }
 
+  ngOnInit(): void {
+    // use lifecycle hook to get service info (better that doing in constructor)
+    this.personList = this.prsService.persons;
+  }
 }
